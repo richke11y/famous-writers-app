16c21d14300087907bd0511a00f21a6cc7bfa8e5
Rich Kelly <rich@richkelly.uk>
2022-08-10 16:16:36 +0100

    Initial commit.

---
+++ stylesApp.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 20,
+		backgroundColor: '#FFFFFF',
+		color: '#000000'
+	}
+});
