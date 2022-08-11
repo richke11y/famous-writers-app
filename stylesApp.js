4af36c6c69c50b99e6b2f2ea2e4f305d27124d36
Rich Kelly <rich@richkelly.uk>
2022-08-11 21:05:03 +0100

    Removed paddingHorizontal from style object. Removed color from style object. Removed fontFamily from style object.

--- stylesApp.js
+++ stylesApp.js
@@ -3,9 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingHorizontal: 20,
 		backgroundColor: '#FFFFFF',
-		color: '#000000',
-		fontFamily: 'HelveticaNeue',
 	}
 });
