432ddd25f8c856e6e3144100d00b74dfff5c8bfa
Rich Kelly <rich@richkelly.uk>
2022-08-15 08:45:56 +0100

    Importing "Header" component. Importing "Footer" component. Importing "AppText" component.

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,32 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import Header from './Header.js';
+import Footer from './Footer.js';
+import AppText from './AppText.js';
 
 import styles from './styles/stylesMain.js';
 
 const Main = () => {
-	return null;
+
+	const { main, mainView } = styles;
+
+	return (
+		<View style={main}>
+
+			<Header />
+
+			<View style={mainView}>
+
+				<AppText text={'William Shakespeare (bapt. 26 April 1564 – 23 April 1616) was an English playwright, poet and actor. He is widely regarded as the greatest writer in the English language and the worlds greatest dramatist. He is often called Englands national poet and the "Bard of Avon" (or simply "the Bard").'}>
+			</AppText>
+
+			</View>
+
+			<Footer />
+
+		</View>
+	);
+
 }
 
 export default Main;
